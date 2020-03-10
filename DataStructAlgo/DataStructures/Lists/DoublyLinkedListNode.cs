684475229a8dbc712a4015fd027a8a928be67f86
Hedydy <sasha.lukashow@gmail.com>
2020-03-11 01:24:29 +0200

    Added QueueArray algoritm

--- DataStructAlgo/DataStructures/Lists/DoublyLinkedListNode.cs
+++ DataStructAlgo/DataStructures/Lists/DoublyLinkedListNode.cs
@@ -1,181 +1,34 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace DataStructAlgo.DataStructures.Lists
 {
-    class DoublyLinkedList<T> : IEnumerable<T>
+    class DoublyLinkedListNode<T>
     {
-        DoublyLinkedListNode<T> _head = null;
+        public DoublyLinkedListNode(T value)
+        {
+            this.Value = value;
+        }
 
-        DoublyLinkedListNode<T> _tail = null;
-
-        public int Count
+        public T Value
         {
             get;
-            private set;
+            set;
         }
 
-        public void AddFirst(T value)
+        public DoublyLinkedListNode<T> Next
         {
-            DoublyLinkedListNode<T> node = new DoublyLinkedListNode<T>(value);
-
-            if (_head == null)
-            {
-                _tail = node;
-            }
-            else
-            {
-                _head.Previous = node;
-                node.Next = _head;
-            }
-
-            _head = node;
-            Count++;
+            get;
+            set;
         }
 
-        public void AddLast(T value)
+        public DoublyLinkedListNode<T> Previous
         {
-            DoublyLinkedListNode<T> node = new DoublyLinkedListNode<T>(value);
-
-            if (_tail == null)
-            {
-                _head = node;
-            }
-            else
-            {
-                _tail.Next = node;
-                node.Previous = _tail;
-            }
-
-            _tail = node;
-            Count++;
-        }
-
-        public void RemoveFirst()
-        {
-            if (Count == 0)
-                return;
-
-            _head = _head.Next;
-
-            if (_head == null)
-            {
-                _tail = null;
-            }
-            else
-            {
-                _head.Previous = null;
-            }
-
-            Count--;
-        }
-
-        public void RemoveLast()
-        {
-            if (Count != 0)
-            {
-                _tail = _tail.Previous;
-
-                if (_tail == null)
-                {
-                    _head = null;
-                }
-                else
-                {
-                    _tail.Next = null;
-                }
-
-                Count--;
-            }
-        }
-
-        public bool Remove(T value)
-        {
-            DoublyLinkedListNode<T> current = _head;
-            DoublyLinkedListNode<T> previous = null;
-
-            while (current != null)
-            {
-                if (current.Value.Equals(value))
-                {
-                    if (previous == null)
-                    {
-                        RemoveFirst();
-                    }
-                    else // Removing from the middle or from the end
-                    {
-
-                        previous.Next = current.Next;
-
-                        if (previous.Next == null)
-                        {
-                            _tail = previous;
-                        }
-                        else
-                        {
-                            current.Next.Previous = previous;
-                        }
-
-                        Count--;
-                    }
-
-                    return true;
-                }
-
-                previous = current;
-                current = current.Next;
-            }
-
-            return false;
-        }
-
-        public IEnumerator<T> GetEnumerator()
-        {
-            DoublyLinkedListNode<T> current = _head;
-
-            while (current != null)
-            {
-                yield return current.Value;
-                current = current.Next;
-            }
-        }
-
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            return (this as IEnumerable<T>).GetEnumerator();
-        }
-
-        public void Clear()
-        {
-            _head = _tail = null;
-            Count = 0;
-        }
-
-        public void CopyTo(T[] array, int index)
-        {
-            DoublyLinkedListNode<T> current = _head;
-
-            while (current != null)
-            {
-                array[index++] = current.Value;
-                current = current.Next;
-            }
-        }
-
-        public bool Contains(T value)
-        {
-            DoublyLinkedListNode<T> current = _head;
-
-            while (current != null)
-            {
-                if (current.Value.Equals(value))
-                {
-                    return true;
-                }
-                current = current.Next;
-            }
-            return false;
+            get;
+            set;
         }
     }
 }
---
+++ DataStructAlgo/DataStructures/Queues/QueueArray.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructAlgo.DataStructures.Queues
+{
+    class QueueArray<T>
+    {
+
+        T[] _array = new T[0];
+
+        int _head;
+        int _tail;
+
+        public int Count
+        {
+            get;
+            private set;
+        }
+
+        public QueueArray() : this(0) { }
+
+        public QueueArray(int capacity)
+        {
+            if(capacity < 0)
+            {
+                throw new ArgumentException();
+            }
+
+            _array = new T[capacity];
+        }
+
+        private void GrowArray()
+        {
+            int newLength = (_array.Length == 0) ? 4 : _array.Length << 1;
+
+            T[] newArray = new T[newLength];
+
+            if(Count > 0)
+            {
+                int index = 0;
+                int i;
+
+                if(_tail < _head)
+                {
+                    for (i = _head; i < _array.Length; i++)
+                    {
+                        newArray[index++] = _array[i];
+                    }
+
+                    for (i = 0; i <= _tail; i++)
+                    {
+                        newArray[index++] = _array[i];
+                    }
+                }
+                else
+                {
+                    for (i = _head; i < _tail; i++)
+                    {
+                        newArray[index++] = _array[i];
+                    }
+                }
+
+                _head = 0;
+                _tail = index - 1;
+            }
+            else
+            {
+                _head = 0;
+                _tail = -1;
+            }
+
+            _array = newArray;
+        }
+
+        public void EnqueueFirst(T value)
+        {
+            if(Count == _array.Length)
+            {
+                GrowArray();
+            }
+
+            if(_head == 0)
+            {
+                _head = _array.Length - 1;
+            }
+            else
+            {
+                _head--;
+            }
+
+            _array[_head] = value;
+
+            Count++;
+        }
+
+        public void EnqueueLast(T value)
+        {
+            if (Count == _array.Length)
+            {
+                GrowArray();
+            }
+
+            if (_tail == _array.Length - 1)
+            {
+                _tail = 0;
+            }
+            else
+            {
+                _tail++;
+            }
+
+            _array[_tail] = value;
+
+            Count++;
+        }
+
+        public T DequeueFirst()
+        {
+            if(Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+
+            if(_head == _array.Length)
+            {
+                _head = 0;
+            }
+
+            Count--;
+
+            return _array[_head++];
+        }
+
+        public T DequeueLast()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+
+            if (_tail < 0)
+            {
+                _tail = _array.Length - 1;
+            }
+
+            Count--;
+
+            return _array[_tail--];
+        }
+
+        public T PeekFirst()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+
+            return _array[_head];
+        }
+
+        public T PeekLast()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+
+            return _array[_tail];
+        }      
+    }
+}
